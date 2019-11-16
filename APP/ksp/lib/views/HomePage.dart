b67d32050672d188cef912a7fd087d9634a6cb80
mohan <themightyking117@gmail.com>
2019-11-16 10:49:12 +0530

    basic fixes

---
+++ APP/ksp/lib/views/HomePage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:ksp/utils/userRepo.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (_) => UserRepository.instance(),
+      child: Consumer(
+        builder: (context, UserRepository user, _) {
+          switch (user.status) {
+            case Status.Uninitialized:
+              return Splash();
+            case Status.Unauthenticated:
+            case Status.Authenticating:
+              return LoginPage();
+            case Status.Authenticated:
+              return UserInfoPage(user: user.user);
+          }
+        },
+      ),
+    );
+  }
+}
