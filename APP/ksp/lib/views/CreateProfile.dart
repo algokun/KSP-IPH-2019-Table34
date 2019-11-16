a6e195290b848eff37a9fb78e36189aae1115c04
mohan <themightyking117@gmail.com>
2019-11-16 17:33:08 +0530

    profile

--- APP/ksp/lib/views/CreateProfile.dart
+++ APP/ksp/lib/views/CreateProfile.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ksp/config/colors.dart';
+import 'package:ksp/models/profileModel.dart';
+import 'package:ksp/utils/profileHandler.dart';
+import 'package:provider/provider.dart';
 
 class CreateProfile extends StatefulWidget {
   @override
   _CreateProfileState createState() => _CreateProfileState();
 }
 
-class _CreateProfileState extends State<CreateProfile> with ColorConfig{
+class _CreateProfileState extends State<CreateProfile> with ColorConfig {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,5 +21,15 @@ class _CreateProfileState extends State<CreateProfile> with ColorConfig{
         elevation: 0.0,
       ),
     );
+  }
+
+  createProfile() {
+    FirebaseUser user = Provider.of<FirebaseUser>(context);
+    UserUpdateInfo info = UserUpdateInfo();
+    info.displayName = "name";
+    info.photoUrl = "phone";
+    user.updateProfile(info);
+    Profile(context).createProfile(ProfileModel(
+        name: 'NAME', phone: 'PHONE', uid: user.uid, userRole: 'ROlE'));
   }
 }
