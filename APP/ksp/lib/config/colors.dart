6635ed5463569b5a755eeb9e916152f99f7997e4
mohan <themightyking117@gmail.com>
2019-11-16 11:49:22 +0530

    added intro

---
+++ APP/ksp/lib/config/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/painting.dart';
+
+mixin ColorConfig{
+  Color primary = Color(0xFFFF9800);
+  Color highContrast = Color(0xFFF5F4F6);
+  Color lowContrast = Color(0xFF54595E);
+  Color contrast = Color(0xFFDDE3E5);
+  Color background = Color(0xFF323538);
+}
--- APP/ksp/lib/main.dart
+++ APP/ksp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ksp/config/colors.dart';
 import 'package:ksp/views/HomePage.dart';
 
 void main() => runApp(MainApp());
@@ -14,14 +15,42 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Nothing extends StatelessWidget {
+class Nothing extends StatefulWidget {
+  @override
+  _NothingState createState() => _NothingState();
+}
+
+class _NothingState extends State<Nothing> with ColorConfig {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Image.asset("assets/bg.jpg")
+          Image.asset(
+            "assets/bg.jpg",
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: height / 3,
+              decoration: BoxDecoration(
+                color: background,
+              ),
+              width: double.infinity,
+              padding: EdgeInsets.all(100.0),
+              child: RaisedButton(
+                color: primary,
+                elevation: 5.0,
+                child: Text("Get Started"),
+                onPressed: () {},
+              ),
+            ),
+          )
         ],
       ),
     );
